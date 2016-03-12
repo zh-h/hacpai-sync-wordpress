ec2ab4583f67976e5dfc8d15c876e5660f6bdec2
zonghua <zh.h@outlook.com>
2016-03-12 17:39:33 +0800

    Update config.php
    hide my personal information

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 /* 填写配置 */
 $client = array('title' => '我的个人博客', //博客抬头
     'host' => 'http://xxx.com', //博客域名
-    'email' => 'zh.h@outlook.com', //需要和 hacpai 的账户一致
+    'email' => 'xxoo@outlook.com', //需要和 hacpai 的账户一致
     'key' => 'zonghua'); //在 https://hacpai.com/settings#soloKey 进行设置
 
 /**
